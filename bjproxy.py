f73e50ea458eecc91f18ea1739ec696fd6ef53f6
b1eberg0n <biebergong@gmail.com>
2016-12-18 21:32:47 +0800

    use multiprocessing

--- bjproxy.py
+++ bjproxy.py
@@ -2,19 +2,22 @@ import re
 import json
 import time
 from urllib.parse import urlparse#, urlunparse
-# from multiprocessing import Process
+import multiprocessing
 # import ssl
-# import socket
+import socket
+import socks
+socks.set_default_proxy(socks.SOCKS5, '192.168.1.1', 1080)
 # from time import sleep
 # import threading
-from gevent.server import StreamServer
-from gevent import sleep, socket
+# from gevent.server import StreamServer
+# from gevent import sleep, socket
 # monkey.patch_socket()
 # monkey.patch_ssl()
 
 def parse_header(raw_headers):
 	request_lines = raw_headers.split('\r\n')
 	first_line = request_lines[0].split(' ')
+	other_lines = request_lines[1:]
 	method = first_line[0]
 	full_path = first_line[1]
 	version = first_line[2]
@@ -30,13 +33,18 @@ def parse_header(raw_headers):
 			address = netloc[:i], int(netloc[i + 1:])
 		else:
 			address = netloc, 80
-	return method, version, scm, address, path, params, query, fragment
+	headers_dict = { 'method':method, 'version':version, 'scm':scm,
+					 'address':address, 'path':path, 'params':params,
+					 'query':query, 'fragment':fragment }
+	headers_list = [ line.split(':') for line in other_lines if line ]
+	headers_dict_more = { line[0].strip():line[1].strip() for line in headers_list }
+	return dict(headers_dict, **headers_dict_more)
 
 
 host_p = re.compile('http://.+?/')
 connection_p = re.compile('Connection: .+?\r\n')
 proxy_p = re.compile('Proxy-.+?\n')
-def make_headers(headers):
+def deal_with_headers(headers):
 	if '\nConnection' in headers:
 		headers = proxy_p.sub('', headers)
 	else:
@@ -47,32 +55,6 @@ def make_headers(headers):
 	headers = '\n'.join(headers)
 	# print(headers)
 	return headers
-
-
-def create_pipe(conn, serv, conn_name='', serv_name=''):
-	serv.settimeout(0.1)
-	conn.settimeout(0.1)
-	while 1:
-		try:
-			for buf in iter(lambda:conn.recv(1024*4),b''):
-				serv.sendall(buf)
-			print('server: {} client: {} close'.format(serv_name, conn_name) )
-			return
-		except socket.timeout:
-			try:
-				for buf in iter(lambda:serv.recv(1024),b''):
-					conn.sendall(buf)
-				print('server: {} client: {} close'.format(serv_name, conn_name) )
-				return
-			except socket.timeout:
-				sleep(0.1)
-				continue
-			except ConnectionResetError:
-				print('server: {} client: {} close'.format(serv_name, conn_name) )
-				return
-		except (ConnectionResetError, BrokenPipeError):
-			print('server: {} client: {} close'.format(serv_name, conn_name) )
-			return	
 
 
 # server_ = json.loads( open('aqua.json').read() )
@@ -86,152 +68,137 @@ def create_pipe(conn, serv, conn_name='', serv_name=''):
 
 
 def httpsproxy(conn, addr, raw_headers):
-	method, version, scm, address, path, params, query, fragment =\
-		parse_header(raw_headers)	
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# s.settimeout(7)
-	# print(pre_dict)
-	# try:
-	s.connect(address)
-	# except socket.timeout:
-	# 	s.settimeout(None)
-	# 	if pre_dict.get(address[0]):
-	# 		black_list[address[0]] = True
-	# 		with open('black.dat', 'w') as f:
-	# 			f.write( '\n'.join( [ i for i in black_list.keys() ] ) )
-	# 	else:
-	# 		pre_dict[address[0]] = True
-	# 	childproxy(conn, raw_headers, conn_name=addr, serv_name=address[0])
-	# 	return
-	# else:
-	# s.settimeout(None)
-	conn.sendall(b'HTTP/1.1 200 Connection established\r\n\r\n')
-	create_pipe(conn, s, conn_name=addr, serv_name=address[0])
-
-
-def httpproxy(conn, addr, headers):
+	headers_dict = parse_header(raw_headers)
+	# serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serv = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		method, version, scm, address, path, params, query, fragment =\
-			parse_header(headers)
-	except:
+		serv.connect(headers_dict['address'])
+	except TimeoutError:
 		return
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# s.settimeout(7)
-	# print(pre_dict)
-	# try:
-	s.connect(address)
-	# except socket.error:
-	# 	print(headers, 'close')
-	# 	s.close()
-	# 	return
-	# except socket.timeout:
-		# s.settimeout(None)
-	# 	if pre_dict.get(address[0]):
-	# 		black_list[address[0]] = True
-	# 		with open('black.dat', 'w') as f:
-	# 			f.write( '\n'.join( [ i for i in black_list.keys() ] ) )
-	# 	else:
-	# 		pre_dict[address[0]] = True
-	# 	childproxy(conn, headers, conn_name=addr, serv_name=address[0])
-	# 	return
-	# else:
-		# s.settimeout(None)
-	print('connect {} success'.format(address[0]))
-	s.settimeout(0.1)
-	conn.settimeout(0.1)
-	raw_headers = headers
-	headers = make_headers(headers)
-	s.sendall(headers.encode())
+	conn.sendall(b'HTTP/1.1 200 Connection established\r\n\r\n')
 	print(addr,
 		  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
-		  raw_headers.split('\r\n')[0])
-	while 1:
-		try:
-			for buf in iter(lambda:s.recv(1024), b''):
-				# print('server:', address[0], len(buf))
-				conn.sendall(buf)
-			# print('server: {} close'.format(address[0]))
-			print('server: {} client: {} close'.format(
-				address[0], addr) )
-			return
-		except socket.timeout:
-			try:
-				while 1:
-					buf = conn.recv(1024)#.decode('utf-8')
-					if b'\r\n\r\n' in buf:
-						buf = buf.split(b'\r\n\r\n')
-						buf[0] = make_headers(buf[0].decode('utf-8','ignore')).encode()#+b'\r\n\r\n'+ buf[1]
-						buf = b'\r\n\r\n'.join(buf)
-						s.sendall(buf)
-						print(addr,
-							  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
-							  raw_headers.split('\r\n')[0])
-					elif buf == b'':
-						# print('client: {} close'.format(addr))
-						print('server: {} client: {} close'.format(address[0], addr) )
-						return
-					else:
-						s.sendall(buf)
-			except socket.timeout:
-				sleep(0.1)
-				continue
-		except BrokenPipeError:
-			# print('client: {} close'.format(addr))
-			print('server: {} client: {} close'.format(address[0], addr) )
-			return
-		except ConnectionResetError:
-			# black_list[address[0]] = True
-			# with open('black.dat', 'w') as f:
-			# 	f.write( '\n'.join( [ i for i in black_list.keys() ] ) )
-			# childproxy(conn, raw_headers, conn_name=addr, serv_name=address[0])
-			return
+		  headers_dict['method'], headers_dict['address'],
+		  headers_dict['path'])
+	# create_pipe(conn, s, conn_name=addr, serv_name=address[0])
+	p = multiprocessing.Process(target=a_to_b, args=(conn, serv, addr[0], headers_dict['address']), daemon=True)
+	p.start()
+	a_to_b(serv, conn, headers_dict['address'], addr[0])
+	return p.terminate()
 
 
-def handle(conn, addr):
+def a_to_b(a_conn, b_conn, a_name, b_name):
+	sendall = b_conn.sendall
+	try:
+		[ sendall(buf) for buf in iter(lambda:a_conn.recv(1024*8), b'') ]
+	except (ConnectionResetError, BrokenPipeError) as e:
+		print(e)
+	finally:
+		a_conn.close()
+		b_conn.close()
+		return print('{} -> {} close'.format(a_name, b_name) )
+
+
+def cli_to_serv(serv, cli, serv_name, cli_name):
+	try:
+		while True:
+			headers = get_headers(cli)
+			if headers:
+				headers_dict = parse_header(headers)
+				headers = deal_with_headers(headers)
+				# print(cli_name, headers)
+				print(cli_name,
+					  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
+					  headers_dict['method'], headers_dict['address'],
+					  headers_dict['path'])
+				serv.sendall(headers.encode())
+			else:
+				break
+	except (ConnectionResetError, BrokenPipeError) as e:
+		print(e)
+	finally:
+		serv.close()
+		cli.close()
+		return print('{} -> {} close'.format(cli_name, serv_name))
+
+
+def httpproxy(cli, addr, headers):
+	# socket.socket = socks.socksocket()
+	serv = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
+	headers_dict = parse_header(headers)
+
+	try:
+		serv.connect(headers_dict['address'])
+	except socket.gaierror as e:
+		print(e, headers_dict['address'])
+	print('connect {} success'.format(headers_dict['Host']))
+	headers = deal_with_headers(headers)
+	# print(addr,headers)
+	print(addr,
+		  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
+		  headers_dict['method'], headers_dict['address'],
+		  headers_dict['path'])
+
+	try:
+		serv.sendall(headers.encode())
+	except BrokenPipeError:
+		return
+	p = multiprocessing.Process(target=a_to_b, args=(serv, cli, headers_dict['Host'], addr[0],), daemon=True)
+	p.start()
+	# p = multiprocessing.Process(target=a_to_b, args=(serv, cli, headers_dict['Host'], addr[0],))
+	# a_to_b(serv, cli, headers_dict['Host'], addr[0])
+	# p.start()
+	cli_to_serv(serv, cli, headers_dict['Host'], addr)
+	return p.terminate()
+	
+
+def get_headers(conn):
 	headers = ''
 	for buf in iter( lambda:conn.recv(1).decode('utf-8','ignore'), ''):
 		headers += buf
 		if headers.endswith('\r\n\r\n'):
 			break
+	return headers
 
-	method = headers.split(' ')[0]
-	if len(headers.split('\r\n')) <= 1:
+
+def handle(conn, cli_addr):
+	# while True:
+	headers = get_headers(conn)
+	try:
+		headers_dict = parse_header(headers)
+	except (ValueError, IndexError):
+		return
+	if len(headers_dict.keys()) <= 1:
 		return
 	else:
 		pass
-	if method == 'CONNECT':
-		serv = headers.split('\r\n')[0].split(' ')[1].split(':')[0]
-	else:
-		serv = headers.split('\r\n')[1].split(' ')[1]
 
-	# if black_list.get( serv ):
-	# 	print( serv, 'black' )
-	# 	print(addr[0],
-	# 		  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
-	# 		  headers.split('\r\n')[0])
-	# 	childproxy(conn, headers, conn_name=addr[0])
-	if method == 'CONNECT':
-		print(addr[0],
-			  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
-			  headers.split('\r\n')[0])
-		httpsproxy(conn, addr[0], headers)
+	if headers_dict['method'] == 'CONNECT':
+		# print(cli_addr[0],
+		# 	  '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
+		# 	  headers_dict['method'], headers_dict['cli_address'],
+		# 	  headers_dict['path'])
+		return httpsproxy(conn, cli_addr, headers)
+		# pass
 	else:
 		# try:
-		httpproxy(conn, addr[0], headers)
+		return httpproxy(conn, cli_addr, headers)
 
 	
-# def main1():
-# 	s = socket.socket()
-# 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# 	s.bind(('0.0.0.0', 8087))
-# 	s.listen(1500)
-# 	while 1:
-# 		conn, addr = s.accept()
-# 		# multiprocessing.Process(target=handle,args=(conn,addr)).start()
+def main1():
+	s = socket.socket()
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind(('0.0.0.0', 8080))
+	s.listen(1500)
+
+	# pool = multiprocessing.Pool(4)
+	while 1:
+		conn, cli_addr = s.accept()
+		# handle(conn,addr)
+		multiprocessing.Process(target=handle,args=(conn, cli_addr,)).start()
 # 		threading.Thread(target=handle,args=(conn,addr)).start()
 		
-# main1()
+main1()
 # black_list = { i.strip():True for i in open('black.dat').readlines() }
 # pre_dict = {}
-StreamServer(('0.0.0.0', 8080), handle).serve_forever()
+# StreamServer(('0.0.0.0', 8080), handle).serve_forever()
